1252b64097fb02a330432bc308925398be7867f6
Spirit <samuelacerolozano@gmail.com>
2025-03-14 13:44:51 -0500

    Commit 8: Implementar modo oscuro/claro con JavaScript

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ window.onload = function () {
         bar.style.width = progressValue + "%";
     });
 };
+
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    themeToggle.textContent = body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
